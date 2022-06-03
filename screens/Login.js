de3a9b543d3ccacb6adf6b652f4620e1ffec34a4
sohyun <kshyun0215@gmail.com>
2022-06-04 00:39:46 +0900

    Add login with firebase

--- screens/Login.js
+++ screens/Login.js
@@ -6,22 +6,51 @@ import {
   Image,
   StyleSheet,
   Dimensions,
+  Alert,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import LinearGradient from "react-native-linear-gradient";
+import { singIn, singUp } from "../lib/auth";
 
 const Login = () => {
   const navigation = useNavigation();
-  const onLogin = () => {
-    
-    navigation.navigate("MainStack", {
-      screen: "MainTabs",
-    });
-  };
   const [id, setId] = useState();
   const [pw, setPw] = useState();
 
+
+  const failedMessages = {
+    "auth/wrong-password": "잘못된 비밀번호입니다.",
+    "auth/user-not-found": "존재하지 않는 계정입니다.",
+  }
+
+  const signIn = async() => {
+    const info = {id, pw};
+    try{
+      const user = await singIn(info);
+      console.log(user);
+    }catch(e){
+      const alertMessage = failedMessages[e.code]
+      Alert.alert("로그인 실패", alertMessage);
+    }
+
+    // navigation.navigate("MainStack", {
+    //   screen: "MainTabs",
+    // });
+  };
+  // const signUp = async() => {
+  //   const info = {id, pw};
+  //   try{
+  //     const user = await singUp(info);
+  //     console.log(user);
+  //   }catch(e){
+
+  //   }
+  // }
+
+
+
+  
 
   return (
     <View style={{ width: "100%", height: "100%", backgroundColor: '#F9FAFF', alignItems: 'center', justifyContent: 'center' }}>
@@ -33,13 +62,13 @@ const Login = () => {
       </TextInput>
 
       <TextInput 
-        onChangeText={(text) => setPw(text)}
+        onChangeText={(text) => {setPw(text)}}
         placeholder="비밀번호"
         style={{ width: 263, height: 45, backgroundColor: 'white', borderColor: '#c4c4c4', borderRadius: 25, borderWidth: 0.7, borderWidth: 0.7, marginTop: 8, paddingLeft: 20}}>
       </TextInput>
 
       <TouchableOpacity 
-        onPress={onLogin}
+        onPress={signIn}
         key={"로그인"}
         style={{ width: 263, height: 45, alignItems: "center", justifyContent: "center", marginTop: 30}}>
           <LinearGradient
